0501055777490b471e2e1925ca20beae17c032cf
SuperLee <70315908+SuperLee7@users.noreply.github.com>
2026-04-09 14:32:16 +0500

    init_patch-2

--- CurrentDataBase/init.sql
+++ CurrentDataBase/init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users 
 (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(120) UNIQUE NOT NULL,
+    username VARCHAR(60) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE,
     password_hash VARCHAR(255) NOT NULL, 
-    role VARCHAR(120) NOT NULL            
+    role VARCHAR(60) NOT NULL            
 );
 
 CREATE TABLE IF NOT EXISTS orders 
@@ -13,8 +13,8 @@ CREATE TABLE IF NOT EXISTS orders
     customer_id INTEGER,
     worker_id INTEGER,
     title VARCHAR(255) NOT NULL,
-    status VARCHAR(120) DEFAULT 'open',
-    task_type VARCHAR(120),
+    status VARCHAR(50) DEFAULT 'open',
+    task_type VARCHAR(50),
     description TEXT,
     classes TEXT
 );
@@ -22,19 +22,16 @@ CREATE TABLE IF NOT EXISTS orders
 CREATE TABLE IF NOT EXISTS images 
 (
     id SERIAL PRIMARY KEY,
-    order_id INTEGER ON DELETE CASCADE,
-    file_path TEXT NOT NULL,
-	FOREIGN KEY (order_id) REFERENCES orders(id)
+    order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+    file_path TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS annotations 
 (
     id SERIAL PRIMARY KEY,
-    image_id INTEGER ON DELETE CASCADE,
-    order_id INTEGER ON DELETE CASCADE,
+    image_id INTEGER REFERENCES images(id) ON DELETE CASCADE,
+    order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
     worker_id INTEGER,
     label_data TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY (image_id) REFERENCES images(id),
-	FOREIGN KEY (order_id) REFERENCES orders(id)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
